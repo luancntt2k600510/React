222abaea6515ccf976c0f590565feb0a8c5e8419
luancntt2k600510 <ducluan05102001@gmail.com>
2023-03-30 17:41:40 +0700

    Layout and Route

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+//Layouts
+import {HeaderOnly} from '~/components/Layout'
+
+//Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//Public routes
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/following', component: Following},
+    { path: '/profile', component: Profile},
+    { path: '/search', component: Search},
+    { path: '/upload', component: Upload, layout: HeaderOnly}
+]
+const privateRoutes = [
+
+]
+
+export { publicRoutes, privateRoutes }
