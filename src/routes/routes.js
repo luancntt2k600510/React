14b110c51330bb85fa592609f51d1ce6bc0d6371
luancntt2k600510 <ducluan05102001@gmail.com>
2023-04-24 18:19:44 +0700

    Sidebar #1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import config from '~/config';
 
 //Layouts
-import { HeaderOnly } from '~/layouts'
+import { HeaderOnly } from '~/layouts';
 
 //Pages
 import Home from '~/pages/Home';
@@ -9,16 +9,18 @@ import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 //Public routes
 const publicRoutes = [
     { path: config.routes.home, component: Home },
     { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
     { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
     { path: config.routes.search, component: Search, layout: null },
-    { path: config.routes.upload, component: Upload, layout: HeaderOnly }
-]
+];
 
-const privateRoutes = []
+const privateRoutes = [];
 
-export { publicRoutes, privateRoutes }
+export { publicRoutes, privateRoutes };
